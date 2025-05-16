7b270ba0661dc7968cb41d40fca386497091ecca
Bundet <namebundet2020@gmail.com>
2025-05-16 22:50:15 +0700

    Fix middleware

--- public/index.php
+++ public/index.php
@@ -26,8 +26,7 @@ $app->add(function ($request, $handler) {
         $response = $response
             ->withHeader('Access-Control-Allow-Origin', $origin)
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->withHeader('Access-Control-Allow-Credentials', 'true');
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 
     return $response;
