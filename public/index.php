bcb1a3bdc19e9b48856c0d0836323d322b8462c9
Bundet <namebundet2020@gmail.com>
2025-05-17 10:35:58 +0700

    Fix

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '../');
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 require __DIR__ . '/../src/config.php';
 
