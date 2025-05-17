1766e41de34a8e87a06bb7b84b763d70f71ce2ed
Bundet <namebundet2020@gmail.com>
2025-05-17 10:10:43 +0700

    Fix

--- src/config.php
+++ src/config.php
@@ -1,7 +1,5 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
     function getDBConnection() {
         $host = $_ENV['DB_HOST'];
         $port = $_ENV['DB_PORT'];
